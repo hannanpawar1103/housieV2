7049d7bfbfef947df7dace54eba18455fc612b1a
Pawar Abdul Hannan <hannanpawar1103@gmail.com>
2025-08-28 22:46:18 +0530

    fix: update lockfile

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://housiev2-server.onrender.com", {
   autoConnect: false,
   transports: ["websocket"],
 });
